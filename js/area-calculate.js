0b5bb6a761d4f0d3737b338b08f3145d378649f3
Khan Mohammad Talha <khanmohammadtalha@Khans-MacBook-Air.local>
2023-10-24 08:19:53 +0600

    parallelogram

--- js/area-calculate.js
+++ js/area-calculate.js
@@ -1,36 +1,18 @@
-function areaCalculator() {
-    //get triangle base value
-    const baseInput = document.getElementById('base-input');
-    const baseInputValue = parseFloat(baseInput.value);
-
-    //get triangle height value
-    const baseHeight = document.getElementById('triangle-height');
-    const baseHeightValue = parseFloat(baseHeight.value);
-
-    // triangle calculation 
-    const area = 0.5 * baseInputValue * baseHeightValue;
-    console.log(area)
-}
-
-function rectangleCalculator(){
-    //rectangle width
-    const rectangleWidth = document.getElementById('rectangle-witdh');
-    const rectangleWidthValue = parseFloat(rectangleWidth.value);
-    //rectangle length
-    const rectangleLength = document.getElementById('rectangle-length');
-    const rectangleLengthValue = parseFloat(rectangleLength.value);
-
-    //calc
-    const area = rectangleWidthValue * rectangleLengthValue;
-
-    //area 
-    const rectangleArea = document.getElementById('rectangle-area');
-    rectangleArea.innerText = area;
-}
-
-function calculateParallelogramArea(id){
-    const inputFields = document.getElementById(id);
-    const inputFieldsText = inputFields.value;
-    const inputValue = parseFloat(inputFieldsText);
+function parallelogramCalculator (id){
+    const paraInputValue = document.getElementById(id);
+    const paraInputValueString = paraInputValue.value;
+    const inputValue = parseFloat(paraInputValueString);
     return inputValue;
+};
+function calculateParallelogramArea(){
+    const paraWidth = parallelogramCalculator('para-width');
+    const paraHeight = parallelogramCalculator('para-height');
+
+    const area = paraWidth * paraHeight;
+};
+
+function setElementInnerText(){
+    const element = document.getElementById('para-area');
+    element.innerText = area;
+    console.log(element)
 }
